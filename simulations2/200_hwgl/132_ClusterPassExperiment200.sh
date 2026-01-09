913e52f1f40638622aa5f81bfe7f2b490cc49a80
Jose Angel Sanchez Gomez <josesa@skylark.ib.hpcc.ucr.edu>
2026-01-09 10:28:49 -0800

    sims2: fixing indexing for simulations

--- simulations2/200_hwgl/132_ClusterPassExperiment200.sh
+++ simulations2/200_hwgl/132_ClusterPassExperiment200.sh
@@ -6,12 +6,12 @@
 #SBATCH --mem-per-cpu=1gb
 #SBATCH --time=02:00:00
 #SBATCH --output=200_hwgl/experiments1/logs/output%a.out
-#SBATCH --array=1450-2889
+#SBATCH --array=10-1449
 
 pwd; hostname; date
 
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-echo This is task $SLURM_ARRAY_TASK_ID
+echo This is task $(($SLURM_ARRAY_TASK_ID + 1440))
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 echo a
 echo a
@@ -24,8 +24,8 @@ echo a
 echo a
 
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-echo 3 Running 200_hwgl/123_SimulationScript.R with input $SLURM_ARRAY_TASK_ID 2
-Rscript 200_hwgl/123_SimulationScript.R $SLURM_ARRAY_TASK_ID 2
+echo 3 Running 200_hwgl/123_SimulationScript.R with input $(($SLURM_ARRAY_TASK_ID + 1440)) 2
+Rscript 200_hwgl/123_SimulationScript.R $(($SLURM_ARRAY_TASK_ID + 1440)) 2
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 echo a
 echo a
--- simulations2/300_ipchd/133_ClusterPassExperiment300.sh
+++ simulations2/300_ipchd/133_ClusterPassExperiment300.sh
@@ -6,12 +6,12 @@
 #SBATCH --mem-per-cpu=1gb
 #SBATCH --time=2-00:00:00
 #SBATCH --output=300_ipchd/experiments1/logs/output%a.out
-#SBATCH --array=2890-4329
+#SBATCH --array=10-1449
 
 pwd; hostname; date
 
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-echo This is task 0
+echo This is task $(($SLURM_ARRAY_TASK_ID + 2880))
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 echo a
 echo a
@@ -24,8 +24,8 @@ echo a
 echo a
 
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-echo 3 Running 300_ipchd/123_SimulationScript.R with input $SLURM_ARRAY_TASK_ID 2
-Rscript 300_ipchd/123_SimulationScript.R $SLURM_ARRAY_TASK_ID 2
+echo 3 Running 300_ipchd/123_SimulationScript.R with input $(($SLURM_ARRAY_TASK_ID + 2880)) 2
+Rscript 300_ipchd/123_SimulationScript.R $(($SLURM_ARRAY_TASK_ID + 2880)) 2
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 echo a
 echo a
